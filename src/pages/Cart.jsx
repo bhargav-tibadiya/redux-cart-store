f557397d9767bb2aaf1f84fda1a9cebd7b19f7f3
Bhargav-Tibadiya <bhargavptibadiya.tbp@gmail.com>
2024-06-03 07:41:47 +0530

    Cart Done

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,6 +1,63 @@
+import { useSelector } from "react-redux"
+import { Link } from "react-router-dom";
+import CartItems from "../components/CartItems";
+import { useEffect, useState } from "react";
+
 const Cart = () => {
+
+  const cart = useSelector((state) => state);
+
+  const [total, setTotal] = useState(0)
+
+  useEffect(() => {
+    setTotal(cart.reduce((acc, curr) => acc + curr, 0))
+  }, [cart])
+
+
   return (
-    <div>Cart</div>
+    <div className="cart_container">
+      {
+        cart.lengthh > 0 ?
+          (
+            <div className="cart_box">
+              <div className="left_content">
+                {
+                  cart.map((item, index) => {
+                    return <CartItems key={item.id} item={item} itemIndex={index} />
+                  })
+                }
+              </div>
+
+              <div className="right_content">
+                <div>
+                  <div className="right_content_heading">
+                    Your Cart
+                  </div>
+                  <div className="right_content_summary">
+                    Summary
+                  </div>
+                  <p>
+                    <span>
+                      Total Items: {cart.length}
+                    </span>
+                  </p>
+                </div>
+                <div>
+                  <p><span> Total : {total} </span></p>
+                  <button>Check Out</button>
+                </div>
+              </div>
+
+            </div>
+          ) :
+          (
+            <div className="cart_box">
+              <h2> Cart is Empty</h2>
+              <Link to="/">Shop Now</Link>
+            </div>
+          )
+      }
+    </div>
   )
 }
 
